2dfbeb011b4d306e2a88048fb644902a1ced7787
PETER ABBOTT <abbotsayshi@gmail.com>
2024-08-28 17:08:09 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Peter Abbott on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
